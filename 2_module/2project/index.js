ba04bf229688bfa5342771e4b4dc2e7c03e6bd58
Svetlana Krasnonosenkikh <svetik.palchik81@gmail.com>
2023-02-18 22:30:36 +0300

    lesson2 module2

---
+++ 2_module/2project/index.js
@@ -0,0 +1,29 @@
+// let test = 2;
+// let newTest = String(test);
+// console.log( typeof newTest)
+
+// let num = 2;
+// // num = num + 3;
+// num += 3;
+// console.log(num);
+
+// let person = {
+//     age: 22,
+//     isAdmin: true
+// }
+//
+// if(person.age >15) console.log("you are older 15")
+let num = 3;
+switch (num) {
+    case 2:
+        console.log("у тебя 2")
+        break
+    case 41:
+        console.log("у тебя 41")
+        break
+    case 6:
+        console.log("у тебя 6")
+        break
+    default:
+        console.log("не верно")
+}

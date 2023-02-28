a0ace049ded5de8820e9c5419709134ce764034b
Svetlana Krasnonosenkikh <svetik.palchik81@gmail.com>
2023-02-28 22:17:41 +0300

    lesson4 part1

---
+++ 2_module/4project/index.js
@@ -0,0 +1,13 @@
+let array = ['Nik', 'Bob', 32, true];
+let nums = [2, 3, 'fgh']
+array.push(nums)
+
+console.log(array)
+
+// let nums = []
+//
+// for (let i = 0; i < 100; i += 2){
+//     nums.push(i)
+// }
+//
+// console.log(nums)

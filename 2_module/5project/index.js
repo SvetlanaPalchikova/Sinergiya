f07af1393848341183d3fe6b0d7758f60c908004
Svetlana Krasnonosenkikh <svetik.palchik81@gmail.com>
2023-03-06 23:15:25 +0300

    lesson 5 module2

---
+++ 2_module/5project/index.js
@@ -0,0 +1,41 @@
+// let user = {
+//     name: "Nikita",
+//     hobby: "prog",
+//     isMan: true,
+//     test: {
+//         name:"fghj",
+//         check: [1,  2, 34]
+//     }
+// }
+//
+// user.hobby = "dance"
+//
+// console.log(user.test.check[1])
+
+// let city = {
+//     name: 'Moskow',
+//     age: 900,
+//     rayon:[
+//         {name: "Центральный", town: "ghjkk"}
+//         {name: "Юбилейный", town: "щцкуг"}
+//         {name: "Ленинский", town: "апрдф"}
+//     ]
+// }
+
+let  obj = {
+    name: "Nik",
+    age: 100,
+    isMan: true
+}
+
+let  obj1 = {
+    name: "Nik",
+    age: 100,
+    isMan: true
+}
+
+let test = 3
+
+let check = 100
+
+console.log(obj === obj1)

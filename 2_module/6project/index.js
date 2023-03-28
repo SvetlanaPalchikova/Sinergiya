b3c12ef3ad6c56d97afbd15cfd5d5d55c728e318
Svetlana Krasnonosenkikh <svetik.palchik81@gmail.com>
2023-03-28 14:03:29 +0300

    lesson6 async in js 1:15

--- 2_module/6project/index.js
+++ 2_module/6project/index.js
@@ -1,13 +1,55 @@
-const delay = () => {
-    let l = 0
-    for (let i=0; i<10000000000; i++) {
-        l++
+// const delay = () => {
+//     let l = 0
+//     for (let i=0; i<10000; i++) {
+//         l++
+//     }
+// }
+//
+// const sayHI = () => {
+//     console.log("hello")
+// }
+//
+// delay()
+// sayHI()
+
+//первый способ сделать запрос на бекенд с помощью функции fetch(функция, которая принимает данные с сайтов)
+// fetch("https://reqres.in/api/users")
+//     .then((result) => result.json())// получили в json формате
+//     .then((data) => console.log (data))//записали в data - result.json()
+
+//Второй способ с помощью конструкции async/await
+// const getData = async () => {
+//     const res = await fetch("https://reqres.in/api/users");
+//     const objServer = await res.json();
+//     console.log(objServer.data);
+// }
+// getData()
+
+//примеры
+
+// const test = () => {
+//     console.log("2")
+//     return console.log('4')
+// }
+// console.log("1");
+// console.log('3')
+// test()
+// console.log('5')
+
+// в консоли мы увидим 1,3,2,4,5,
+
+const test = () => {
+    const check = () => {
+        console.log("6")
     }
+    console.log("2")
+    check()
+    console.log('5');
+    return console.log('4')
 }
+console.log("1");
+test()
+console.log('3');
 
-const sayHI = () => {
-    console.log("hello")
-}
+//1,2,6,5,4,3
 
-delay()
-sayHI()

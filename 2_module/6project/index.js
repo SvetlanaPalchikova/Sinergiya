a99cc487c139d1be2cc074b5784d9cfda10362d0
Svetlana Krasnonosenkikh <svetik.palchik81@gmail.com>
2023-03-30 19:53:19 +0300

    lesson6 module2 houm work

--- 2_module/6project/index.js
+++ 2_module/6project/index.js
@@ -37,19 +37,62 @@
 // console.log('5')
 
 // в консоли мы увидим 1,3,2,4,5,
+//
+// const test = () => {
+//     const check = () => {
+//         console.log("6")
+//     }
+//     console.log("2")
+//     check()
+//     console.log('5');
+//     return console.log('4')
+// }
+// console.log("1");
+// test()
+// console.log('3');
+//
+// //1,2,6,5,4,3
+//
+// fetch("https://reqres.in/api/users")
+//      .then((result) => result.json())// получили в json формате
+//     .then((data) => console.log (data))//записали в data - result.json()
+// console.log()
+fetch('https://reqres.in/api/users')
+    .then((response) => {
+        console.log(response);
+        return response.json();
+    }).then((body) => {
 
-const test = () => {
-    const check = () => {
-        console.log("6")
-    }
-    console.log("2")
-    check()
-    console.log('5');
-    return console.log('4')
-}
-console.log("1");
-test()
-console.log('3');
+    console.log('-----------');
+    console.log('Пункт №1:')
+    console.log('-----------');
+    console.log(body);
 
-//1,2,6,5,4,3
+    console.log('-----------');
+    console.log('Пункт №2:')
+    console.log('-----------');
+    body.data.forEach(element => {
+        console.log(element.last_name)
+    });
 
+    console.log('-----------');
+    console.log('Пункт №3:')
+    console.log('-----------');
+    body.data.filter(item=>item.last_name.startsWith('F')).forEach(element => {
+        console.log(element.last_name)
+    });
+
+    console.log('-----------');
+    console.log('Пункт №4:')
+    console.log('-----------');
+    console.log(body.data.reduce((acc, item)=>acc + item.first_name + " " + item.last_name +', ', "Наша база содержит данные следующих пользователей:"));
+
+    console.log('-----------');
+    console.log('Пункт №5:')
+    console.log('-----------');
+    Object.keys(body).forEach(element=>
+        console.log(element)
+    );
+
+
+});

41a685417ae72cb57cf5675a51e2412d9ee4fa1b
Svetlana Krasnonosenkikh <svetik.palchik81@gmail.com>
2023-04-29 00:02:27 +0300

    lesson7 module2 1h 31m

--- 2_module/7project/index.js
+++ 2_module/7project/index.js
@@ -5,12 +5,67 @@
 
 // console.log(divGroup)
 
-let list = document.getElementById("list")
+// let list = document.getElementById("list")
+//
+// let button = document.getElementById("btn")
+//
+// function modifyText() {
+//     button.textContent = "На меня нажали, ура!";
+// }
+// button.addEventListener(" ", modifyText )
+// list.innerHTML = "<li><a href=#>Click</a></li><li><a href=#>Click</a></li><li><a href=#>Click</a></li>";
 
-let button = document.getElementById("btn")
+//меняем цвет квадрата
+// let testDiv = document.getElementById("test");
+// let btn = document.getElementById("btn");
+//
+// const editDivColor = () => {
+//     testDiv.classList.add("upgrade");
+// };
+//
+// btn.addEventListener("click", editDivColor);
 
-function modifyText() {
-    button.textContent = "На меня нажали, ура!";
-}
-button.addEventListener("btn", modifyText )
-list.innerHTML = "<li><a href=#>Click</a></li><li><a href=#>Click</a></li><li><a href=#>Click</a></li>";
+//пишем счетчик
+// let input = document.getElementById("input")
+// let check = document.getElementById("check")
+// let button = document.getElementById("btn")
+//
+// const plusMin = () => {
+//     if (check.checked) {
+//         input.value = +input.value - 15;
+//     } else {
+//         input.value = +input.value + 15;
+//     }
+// }
+//
+// button.addEventListener("click", plusMin)
+
+// пишем калькулятор
+
+let input1 = document.getElementById('input1')
+let input2 = document.getElementById('input2')
+let button = document.getElementById('btn')
+let button1 = document.getElementById('btn1')
+let button2 = document.getElementById('btn2')
+let button3 = document.getElementById('btn3')
+let ask = document.getElementById('ask')
+
+const sum = () => {
+    ask.textContent = Number(input1.value) + +input2.value;
+};
+button.addEventListener("click", sum );
+
+const sub = () => {
+    ask.textContent = Number(input1.value) - +input2.value;
+};
+button1.addEventListener("click", sub );
+
+const mult = () => {
+    ask.textContent = Number(input1.value) * +input2.value;
+};
+button2.addEventListener("click", mult );
+
+const div = () => {
+    ask.textContent = Number(input1.value) / +input2.value;
+};
+button3.addEventListener("click", div );

7253d1b0a22cdc1ba5cc696f21befce427074d70
Svetlana Krasnonosenkikh <svetik.palchik81@gmail.com>
2023-04-29 19:27:09 +0300

    houmwork7 module2 task1

--- 2_module/7project/index.js
+++ 2_module/7project/index.js
@@ -42,30 +42,53 @@
 
 // пишем калькулятор
 
-let input1 = document.getElementById('input1')
-let input2 = document.getElementById('input2')
-let button = document.getElementById('btn')
-let button1 = document.getElementById('btn1')
-let button2 = document.getElementById('btn2')
-let button3 = document.getElementById('btn3')
-let ask = document.getElementById('ask')
+// let input1 = document.getElementById('input1')
+// let input2 = document.getElementById('input2')
+// let button = document.getElementById('btn')
+// let button1 = document.getElementById('btn1')
+// let button2 = document.getElementById('btn2')
+// let button3 = document.getElementById('btn3')
+// let ask = document.getElementById('ask')
+//
+// const sum = () => {
+//     ask.textContent = Number(input1.value) + +input2.value;
+// };
+// button.addEventListener("click", sum );
+//
+// const sub = () => {
+//     ask.textContent = Number(input1.value) - +input2.value;
+// };
+// button1.addEventListener("click", sub );
+//
+// const mult = () => {
+//     ask.textContent = Number(input1.value) * +input2.value;
+// };
+// button2.addEventListener("click", mult );
+//
+// const div = () => {
+//     ask.textContent = Number(input1.value) / +input2.value;
+// };
+// button3.addEventListener("click", div );
 
-const sum = () => {
-    ask.textContent = Number(input1.value) + +input2.value;
-};
-button.addEventListener("click", sum );
+let input = document.getElementById("input")
+let text = document.getElementById('h2')
+document.body.append(input, text)
+const textInput = () => {
+    text.textContent = input.value
+}
+input.addEventListener("keypress", () => {
+    setTimeout(() => {
+        text.textContent = input.value
+    }, 300)
+})
 
-const sub = () => {
-    ask.textContent = Number(input1.value) - +input2.value;
-};
-button1.addEventListener("click", sub );
-
-const mult = () => {
-    ask.textContent = Number(input1.value) * +input2.value;
-};
-button2.addEventListener("click", mult );
-
-const div = () => {
-    ask.textContent = Number(input1.value) / +input2.value;
-};
-button3.addEventListener("click", div );
+// Готовое решение из инета
+// const contentText = document.createElement('h2');
+// const inputText = document.createElement('input');
+// document.body.append(contentText,inputText)
+//
+// inputText.addEventListener('keypress', () => {
+//     setTimeout(() => {
+//         contentText.textContent = inputText.value;
+//     },300)
+// })

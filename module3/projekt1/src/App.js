614ce5b17ae8b9830d812180452ebbfc60e554bd
Svetlana Krasnonosenkikh <svetik.palchik81@gmail.com>
2023-05-22 21:09:19 +0300

    module3 homework

---
+++ module3/projekt1/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      Hello react!
+        <div className="App-div">
+          <ul>Фреймворк</ul>
+          <ul>Реакт</ul>
+          <ul>ES6</ul>
+          <ul>Компоненты</ul>
+          <ul>JSX</ul>
+        </div>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;

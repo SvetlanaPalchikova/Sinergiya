7c080e197c78834d9abaf9fe15bc39dd6f335981
Svetlana Krasnonosenkikh <svetik.palchik81@gmail.com>
2023-05-25 13:07:00 +0300

    module3 HW2

--- module3/projekt1/src/App.js
+++ module3/projekt1/src/App.js
@@ -1,19 +1,39 @@
 import './App.css';
+import React, {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      Hello react!
-        <div className="App-div">
-          <ul>Фреймворк</ul>
-          <ul>Реакт</ul>
-          <ul>ES6</ul>
-          <ul>Компоненты</ul>
-          <ul>JSX</ul>
-        </div>
-      </header>
+    const[count, setCount]= useState(0);
+    const [date, setDate] = useState(new Date());
 
+    useEffect(() => {
+        const timerID = setInterval(() => tick(), 1000);
+
+        return function cleanup() {
+            clearInterval(timerID);
+        };
+    });
+
+    function tick() {
+        setDate(new Date());
+    }
+
+    const options = {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        timeZoneName: 'short'
+    };
+    return (
+    <div >
+        <p>{date.toLocaleString('en', options)}</p>
+        <p>{date.toLocaleString('ru', options)}</p>
+      <p>Счётчик:{count} </p>
+        <button onClick={()=> setCount(count+1)}>Добавить</button>
+        <button onClick={()=> setCount(count-1)}>Уменьшить</button>
     </div>
   );
 }

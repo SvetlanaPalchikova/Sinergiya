ec80b41720343230e04ceea1d899a5cc75fef887
Svetlana Krasnonosenkikh <svetik.palchik81@gmail.com>
2023-06-21 17:44:46 +0300

    task3 module3

--- module3/projekt1/src/App.js
+++ module3/projekt1/src/App.js
@@ -1,40 +1,15 @@
 import './App.css';
-import React, {useEffect, useState} from "react";
+import React from "react";
+import Welcome from "./Welcome";
+
 
 function App() {
-    const[count, setCount]= useState(0);
-    const [date, setDate] = useState(new Date());
-
-    useEffect(() => {
-        const timerID = setInterval(() => tick(), 1000);
-
-        return function cleanup() {
-            clearInterval(timerID);
-        };
-    });
-
-    function tick() {
-        setDate(new Date());
-    }
-
-    const options = {
-        weekday: 'long',
-        month: 'long',
-        day: 'numeric',
-        year: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric',
-        timeZoneName: 'short'
-    };
     return (
-    <div >
-        <p>{date.toLocaleString('en', options)}</p>
-        <p>{date.toLocaleString('ru', options)}</p>
-      <p>Счётчик:{count} </p>
-        <button onClick={()=> setCount(count+1)}>Добавить</button>
-        <button onClick={()=> setCount(count-1)}>Уменьшить</button>
-    </div>
+    <>
+       <Welcome name={"Аня"}/>
+       <Welcome name={"Вера"}/>
+       <Welcome name={"Ира"}/>
+    </>
   );
 }
 

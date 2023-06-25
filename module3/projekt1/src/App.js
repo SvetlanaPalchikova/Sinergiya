5a775f4a3b1cc9b1dde5f99d599a4337778a131f
Svetlana Krasnonosenkikh <svetik.palchik81@gmail.com>
2023-06-25 21:53:06 +0300

    lesson4 task1

--- module3/projekt1/src/App.js
+++ module3/projekt1/src/App.js
@@ -1,16 +1,46 @@
-import './App.css';
 import React from "react";
-import Welcome from "./Welcome";
-
+import s from "./App.module.css";
 
 function App() {
     return (
-    <>
-       <Welcome name={"Аня"}/>
-       <Welcome name={"Вера"}/>
-       <Welcome name={"Ира"}/>
-    </>
-  );
+        <div className={s.container}>
+            <div className={s.div}><h1>Pегистрация</h1>
+                <form>
+                    <div >
+                        <label>Имя: </label>
+                        <input type="text" className={s.input}/>
+                    </div>
+                    <div >
+                        <label>Фамилия: </label>
+                        <input type="text" className={s.input}/>
+                    </div>
+                    <div >
+                        <label>Отчество: </label>
+                        <input type="text" className={s.input}/>
+                    </div>
+                    <div >
+                        <label>Дата рождения: </label>
+                        <input type="date" className={s.input}/>
+                    </div>
+                    <div className={s.input}>
+                        <label>пол: </label>
+                        <label>жен</label>
+                        <input
+                            type="radio"
+                            name="gender"
+                        />
+                        <label>муж</label>
+                        <input
+                            type="radio"
+                            name="gender"
+                        /></div>
+                    <div className={s.container} >
+                        <input type="submit" className={s.input_submit}/>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default App;

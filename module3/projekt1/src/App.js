14af6608a84a9b1f8ef52d37a3191c6ad17427d8
Svetlana Krasnonosenkikh <svetik.palchik81@gmail.com>
2023-06-27 22:27:44 +0300

    module 3 lesson 4 task 2

--- module3/projekt1/src/App.js
+++ module3/projekt1/src/App.js
@@ -1,39 +1,42 @@
 import React from "react";
 import s from "./App.module.css";
+import useInput from './UseInput.js'
 
 function App() {
+    const name = useInput('', true)
     return (
         <div className={s.container}>
             <div className={s.div}><h1>Pегистрация</h1>
                 <form>
                     <div >
                         <label>Имя: </label>
-                        <input type="text" className={s.input}/>
+                        <input {...name} type="text" className={s.input}/>
+                     {name.error && <span style={{ color: 'red'}}>{name.error}</span>}
                     </div>
                     <div >
                         <label>Фамилия: </label>
-                        <input type="text" className={s.input}/>
+                         <input {...name}  type="text" className={s.input}/>
+                        {name.error && <span style={{ color: 'red'}}>{name.error}</span>}
                     </div>
                     <div >
                         <label>Отчество: </label>
-                        <input type="text" className={s.input}/>
+                        <input  {...name} type="text" className={s.input}/>
+                     {name.error && <span style={{ color: 'red'}}>{name.error}</span>}
                     </div>
                     <div >
                         <label>Дата рождения: </label>
-                        <input type="date" className={s.input}/>
+                        <input {...name} type="date" className={s.input}/>
+                     {name.error && <span style={{ color: 'red'}}>{name.error}</span>}
                     </div>
                     <div className={s.input}>
                         <label>пол: </label>
                         <label>жен</label>
-                        <input
-                            type="radio"
-                            name="gender"
+                        <input type="radio" name="gender"
                         />
                         <label>муж</label>
-                        <input
-                            type="radio"
-                            name="gender"
-                        /></div>
+                        <input type="radio" name="gender"
+                        />
+                    </div>
                     <div className={s.container} >
                         <input type="submit" className={s.input_submit}/>
                     </div>

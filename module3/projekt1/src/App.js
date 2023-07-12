51d2928e45b0b7755e007cce74203dcad7c96ffc
Svetlana Krasnonosenkikh <svetik.palchik81@gmail.com>
2023-07-12 22:26:16 +0300

    module3 lesson5

--- module3/projekt1/src/App.js
+++ module3/projekt1/src/App.js
@@ -1,82 +1,18 @@
-import React, {useEffect, useState} from "react";
-
-// import s from "./App.module.css";
-// import useInput from './UseInput.js'
-//
-// function App() {
-//     const name = useInput('', true)
-//     return (
-//         <div className={s.container}>
-//             <div className={s.div}><h1>Pегистрация</h1>
-//                 <form>
-//                     <div >
-//                         <label>Имя: </label>
-//                         <input {...name} type="text" className={s.input}/>
-//                      {name.error && <span style={{ color: 'red'}}>{name.error}</span>}
-//                     </div>
-//                     <div >
-//                         <label>Фамилия: </label>
-//                          <input {...name}  type="text" className={s.input}/>
-//                         {name.error && <span style={{ color: 'red'}}>{name.error}</span>}
-//                     </div>
-//                     <div >
-//                         <label>Отчество: </label>
-//                         <input  {...name} type="text" className={s.input}/>
-//                      {name.error && <span style={{ color: 'red'}}>{name.error}</span>}
-//                     </div>
-//                     <div >
-//                         <label>Дата рождения: </label>
-//                         <input {...name} type="date" className={s.input}/>
-//                      {name.error && <span style={{ color: 'red'}}>{name.error}</span>}
-//                     </div>
-//                     <div className={s.input}>
-//                         <label>пол: </label>
-//                         <label>жен</label>
-//                         <input type="radio" name="gender"
-//                         />
-//                         <label>муж</label>
-//                         <input type="radio" name="gender"
-//                         />
-//                     </div>
-//                     <div className={s.container} >
-//                         <input type="submit" className={s.input_submit}/>
-//                     </div>
-//                 </form>
-//             </div>
-//         </div>
-//     );
-// }
-
-import List from "./component/List";
-import withListLoading from "./component/withListLoading";
+import React, { useState} from "react";
+import Form from "./Form.js"
 
 function App() {
-    const ListLoading = withListLoading(List);
-    const [appState, setAppState] = useState({
-        loading: false,
-        repos: null,
-    })
+const [show, setShow] = useState(false)
 
-    useEffect(() => {
-        setAppState({ loading: true})
-        const apiUrl = 'http://api.github.com/users/hacktivist123/repos'
-        fetch(apiUrl)
-            .then((res) => res.json())
-            .then((repos) => {
-                setAppState({loading: false, repos: repos})
-            })
-    }, [setAppState])
     return(
         <div>
-            <div>
-                <h1>Мои репозитории</h1>
-            </div>
-            <div>
-                <ListLoading isLoading={appState.loading}
-                             repos={appState.repos}/>
-            </div>
+            <button onClick={() => setShow(!show)}>
+                {show ? 'Скрыть' : 'Зарегистрироваться'}
+            </button>
+            {show && <Form/>}
         </div>
     )
+
 }
 
 export default App;
